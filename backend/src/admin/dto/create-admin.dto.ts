675fadc738a3a68f8b77cd8af100d96c9f352fc5
kamaldeen <kamalaliyu503@gmail.com>
2025-04-25 09:58:13 +0100

    added dashboard for course, user, and payment management

---
+++ backend/src/admin/dto/create-admin.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsString, MinLength, IsEnum, IsOptional } from 'class-validator';
+import { UserRole } from '../../roles/roles.enum';
+
+export class CreateAdminDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
+} 
