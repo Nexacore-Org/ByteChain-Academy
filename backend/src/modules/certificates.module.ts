db2d0ab5bd080de44c5b0bb89353bd9040392c36
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-31 09:03:32 +0100

    feat: proof of learning certificate

--- backend/src/modules/certificates.module.ts
+++ backend/src/modules/certificates.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Certificate } from '../entities/certificate.entity';
+import { User } from '../entities/user.entity';
+import { Course } from '../entities/course.entity';
 import { CertificateService } from '../services/certificate.service';
 import { CertificateController } from '../controllers/certificate.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Certificate])],
+  imports: [
+    TypeOrmModule.forFeature([Certificate, User, Course]),
+  ],
   controllers: [CertificateController],
   providers: [CertificateService],
   exports: [CertificateService],
