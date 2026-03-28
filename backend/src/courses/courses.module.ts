e72349738f06d149b90d62dcf1f754ff5b50f456
clintjeff2 <clintjeff2@gmail.com>
2026-03-28 05:36:21 +0100

    fix: remove unused AuthModule import and add LessonsModule with forwardRef to break circular dependency

--- backend/src/courses/courses.module.ts
+++ backend/src/courses/courses.module.ts
@@ -5,15 +5,15 @@ import { CoursesController } from './courses.controller';
 import { AdminCoursesController } from '../admin/admin-courses.controller';
 import { CoursesService } from './courses.service';
 import { CourseRegistration } from './entities/course-registration.entity';
-import { AuthModule } from '../auth/auth.module';
 import { PaginationService } from '../common/services/pagination.service';
+import { LessonsModule } from '../lessons/lessons.module';
 import { Lesson } from '../lessons/entities/lesson.entity';
 import { Progress } from '../progress/entities/progress.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Course, CourseRegistration, Lesson, Progress]),
-    AuthModule,
+    forwardRef(() => LessonsModule),
   ],
   controllers: [CoursesController, AdminCoursesController],
   providers: [CoursesService, PaginationService],
